f161bb327bdfd5141fa415afd32d5613b58faa1c
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-10-25 16:00:31 +0100

    Intégration du module de gestion des rôles et création d'un middleware pour le checking des rôles.

---
+++ app/Console/Commands/CreateSuperAdmin.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CreateSuperAdmin extends Command
+{
+    protected $signature = 'app:create-super-admin';
+
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        //
+    }
+}

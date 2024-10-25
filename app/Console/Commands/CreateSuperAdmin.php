8620ba54b0c4f26b015b50da440eee63d4c28fd9
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-10-25 17:00:57 +0100

    Ajout de la commande console pour superadmin

--- app/Console/Commands/CreateSuperAdmin.php
+++ app/Console/Commands/CreateSuperAdmin.php
@@ -2,16 +2,37 @@
 
 namespace App\Console\Commands;
 
+use App\Models\User;
 use Illuminate\Console\Command;
+use Spatie\Permission\Models\Role;
 
 class CreateSuperAdmin extends Command
 {
-    protected $signature = 'app:create-super-admin';
+    protected $signature = 'make:superadmin {name} {email} {password}';
 
-    protected $description = 'Command description';
+    protected $description = 'Commande console pour la creation d\'un super administrateur.';
 
     public function handle()
     {
-        //
+        $name = $this->argument('name');
+        $email = $this->argument('email');
+        $password = bcrypt($this->argument('password'));
+
+
+        // Créer l'utilisateur
+        $user = User::firstOrCreate([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+
+        ]);
+
+        // Vérifier si le rôle super-admin existe, sinon le créer
+        $role = Role::firstOrCreate(['name' => 'superadmin']);
+
+        // Associer le rôle à l'utilisateur
+        $user->roles()->attach($role->id);
+
+        $this->info("Super-admin créé : {$user->name} ({$user->email}) ");
     }
 }

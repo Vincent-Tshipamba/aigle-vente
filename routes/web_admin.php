be37bcfbab7c9ecfcb9e5d300f6ef6d12cb48694
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2024-10-27 12:32:32 +0100

    Fix dev

--- routes/web_admin.php
+++ routes/web_admin.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RolePermissionController;
 
-Route::get('/dashboard', function () {
+Route::get('/admin/dashboard', function () {
     return view('admin.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->middleware('checkRole:superadmin')->name('dashboard');
 
 Route::resource('users', UserController::class);
 

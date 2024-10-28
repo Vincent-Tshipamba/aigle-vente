42eab3b266fe5e415fc5250bfd260055c0bdf164
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-10-28 17:00:25 +0100

    Ajout du controller pour les categories de produits

---
+++ app/Http/Controllers/Admin/CategoryProductController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\CategoryProduct;
+use Illuminate\Http\Request;
+
+class CategoryProductController extends Controller
+{
+    public function index()
+    {
+        $categories = CategoryProduct::all();
+        return view('admin.categories.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CategoryProduct $categoryProduct)
+    {
+        return view('admin.categories.show', compact('categoryProduct'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CategoryProduct $categoryProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CategoryProduct $categoryProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CategoryProduct $categoryProduct)
+    {
+        //
+    }
+}

555e9ef040ea0122b5b72f35e54861b529a0e7a8
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-10-28 16:19:45 +0100

    Modification du tableau d'affichage des utilisateurs et ajout de l'option d'activation et desactivation.

--- resources/views/components/users-table.blade.php
+++ resources/views/components/users-table.blade.php
@@ -2,13 +2,49 @@
 <div class="card">
     <div class="card-body">
         <div class="table-responsive">
-            <table border="1" id="search-table" class="table table-striped table-bordered">
+            <table border="1" id="users-table" class="table table-striped table-bordered">
                 <thead>
                     <tr>
-                        <th>#</th>
-                        <th>Nom d'utilisateur</th>
-                        <th>Email</th>
-                        <th>Role</th>
+                        <th>
+                            <span class="flex items-center">
+                                #
+                                <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
+                                </svg>
+                            </span>
+                        </th>
+                        <th>
+                            <span class="flex items-center">
+                                Nom d'utilisateur
+                                <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
+                                </svg>
+                            </span>
+                        </th>
+                        <th>
+                            <span class="flex items-center">
+                                Email
+                                <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
+                                </svg>
+                            </span>
+                        </th>
+                        <th>
+                            <span class="flex items-center">
+                                Role
+                                <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
+                                </svg>
+                            </span>
+                        </th>
                         <th>Action</th>
                     </tr>
                 </thead>
@@ -20,20 +56,16 @@
                             <td>{{ $user->email }}</td>
                             <td class="">
                                 @foreach ($roles as $role)
-                                        {{ $user->roles->contains($role) ? $role->name : '' }}
+                                    {{ $user->roles->contains($role) ? $role->name : '' }}
                                 @endforeach
                             </td>
                             <td>
-                                <a href="{{ route('users.destroy', $user->id) }}"
-                                    onclick="remove(event, '{{$user->id}}', '{{ $user->name }}')">
-                                    <svg class="w-6 h-6 text-black dark:text-white hover:text-red-500 dark:hover:text-red-500" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        fill="currentColor" viewBox="0 0 24 24">
-                                        <path fill-rule="evenodd"
-                                            d="M5 8a4 4 0 1 1 8 0 4 4 0 0 1-8 0Zm-2 9a4 4 0 0 1 4-4h4a4 4 0 0 1 4 4v1a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-1Zm13-6a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-4Z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                </a>
+                                <label class="inline-flex items-center cursor-pointer">
+                                    <input type="checkbox" value="" class="sr-only peer" checked>
+                                    <div
+                                        class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#e38407]">
+                                    </div>
+                                </label>
                             </td>
                         </tr>
                     @endforeach

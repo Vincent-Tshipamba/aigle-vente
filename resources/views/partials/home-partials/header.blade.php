519679c71004e4c0404c105170ca5b437b93baaf
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2024-11-02 21:26:08 +0100

    Organisation de l'en-tete

--- resources/views/partials/home-partials/header.blade.php
+++ resources/views/partials/home-partials/header.blade.php
@@ -38,27 +38,13 @@
                                     <i class="fal fa-shopping-cart"></i>
                                     <span class="tp-product-count">2</span>
                                 </button>
+
+                                <a href="wishlist.html"><i class="fal fa-heart"></i></a>
                                 @auth
                                     @php
                                         $user = Auth::user();
                                         $client = App\Models\Client::where('user_id', $user->id)->first();
                                     @endphp
-
-                                    @if (Auth::check() && !Auth::user()->isSeller())
-                                        <a href="{{ route('sellers.create') }}" class="tptrack__submition">Devenir
-                                            vendeur</a>
-                                    @else
-                                        <a href="{{ route('seller.dashboard') }}">
-                                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                fill="currentColor" viewBox="0 0 24 24">
-                                                <path fill-rule="evenodd"
-                                                    d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm16 7H4v7h16v-7ZM5 8a1 1 0 0 1 1-1h.01a1 1 0 0 1 0 2H6a1 1 0 0 1-1-1Zm4-1a1 1 0 0 0 0 2h.01a1 1 0 0 0 0-2H9Zm2 1a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2H12a1 1 0 0 1-1-1Z"
-                                                    clip-rule="evenodd" />
-                                            </svg>
-                                        </a>
-                                    @endif
-
                                     <div class="flex items-center mx-3 space-x-5 "
                                         data-dropdown-toggle="dropdown-user-header">
                                         <div>
@@ -108,12 +94,25 @@
                                             </ul>
                                         </div>
                                     </div>
+                                    @if (Auth::check() && !Auth::user()->isSeller())
+                                        <a href="{{ route('sellers.create') }}"
+                                            class="w-full inline-block px-[10px] py-[15px] bg-[var(--tp-text-primary)] text-[var(--tp-common-white)] text-sm font-semibold rounded-md text-center transition-all duration-300 whitespace-nowrap align-middle touch-manipulation">
+                                            Devenir vendeur</a>
+                                    @else
+                                        <a href="{{ route('seller.dashboard') }}">
+                                            <svg class=" text-gray-800 dark:text-white" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                fill="currentColor" viewBox="0 0 24 24">
+                                                <path fill-rule="evenodd"
+                                                    d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm16 7H4v7h16v-7ZM5 8a1 1 0 0 1 1-1h.01a1 1 0 0 1 0 2H6a1 1 0 0 1-1-1Zm4-1a1 1 0 0 0 0 2h.01a1 1 0 0 0 0-2H9Zm2 1a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2H12a1 1 0 0 1-1-1Z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </a>
+                                    @endif
                                 @else
                                     <a href="{{ route('login') }}"><i class="fal fa-user"></i></a>
                                     <!-- Lien vers le formulaire de connexion -->
                                 @endauth
-
-                                <a href="wishlist.html"><i class="fal fa-heart"></i></a>
                             </div>
                         </div>
                     </div>

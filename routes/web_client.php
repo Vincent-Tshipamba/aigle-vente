ac819929dc3a2c81dd86ebc6bebe5edf6468f797
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2024-11-04 15:49:41 +0100

    Ajout de la route pour le dashboard client

--- routes/web_client.php
+++ routes/web_client.php
@@ -41,4 +41,5 @@ Route::middleware(['auth', 'userOnline'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('client.dashboard');
 });

18612682e33ba7884ddaf4c2459385f48e91b7f2
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-12-16 23:11:43 +0100

    Amelioration complete du tableau de bord pour le client.

--- app/Http/Controllers/Client/DashboardController.php
+++ app/Http/Controllers/Client/DashboardController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Client;
 
+use App\Models\Message;
 use App\Models\Order;
 use App\Models\Client;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Wishlist;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
@@ -13,28 +15,48 @@ class DashboardController extends Controller
     public function index()
     {
         $client = Client::where('_id', Auth::user()->client->_id)->first();
-        $recentOrders = Order::where('client_id', $client->id)->get();
+        $recentWishlistProducts = Wishlist::where('user_id', Auth::user()->id)->get();
         $totalProductsInWishlist = 0;
 
-        return view('client.dashboard', compact('totalProductsInWishlist', 'client', 'recentOrders'));
+        return view('client.dashboard', compact('totalProductsInWishlist', 'client', 'recentWishlistProducts'));
     }
 
-    public function getOrdersWishlistByPeriod(Request $request)
+    public function getClientWishlistByPeriod(Request $request)
     {
         $year = $request->input('year');
         $month = $request->input('month');
 
-        $query = Client::selectRaw("date_format(created_at, '%Y-%m-%d') as date, count(*) as aggregate")
-        ->whereYear('created_at', $year);
+        $query = Wishlist::selectRaw("date_format(created_at, '%Y-%m-%d') as date, count(*) as aggregate")
+            ->where('user_id', Auth::user()->id)
+            ->whereYear('created_at', $year);
 
 
         if ($month && $month !== 'all') {
             $query->whereMonth('created_at', $month);
         }
 
-        $orders = $query->groupBy('date')->get();
+        $wishlist = $query->groupBy('date')->get();
 
-        return response()->json($orders);
+        return response()->json(data: $wishlist);
+    }
+
+    public function getClientContactedSellersByPeriod(Request $request)
+    {
+        $year = $request->input('year');
+        $month = $request->input('month');
+
+        $query = Message::selectRaw("date_format(created_at, '%Y-%m-%d') as date, count(*) as aggregate")
+            ->where('sender_id', Auth::user()->id)
+            ->whereYear('created_at', $year);
+
+
+        if ($month && $month !== 'all') {
+            $query->whereMonth('created_at', $month);
+        }
+
+        $contactedSellers = $query->groupBy('date')->get();
+
+        return response()->json(data: $contactedSellers);
     }
 
     public function wishlist()
--- resources/views/client/layouts/app.blade.php
+++ resources/views/client/layouts/app.blade.php
@@ -17,7 +17,9 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="body bg-white dark:bg-[#1E293B] text-black">
+<body class="body bg-white dark:bg-[#1E293B] text-black" x-data="{ page: 'ecommerce', 'loaded': true, 'darkMode': true, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }" x-init="darkMode = JSON.parse(localStorage.getItem('darkMode'));
+$watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
+    :class="{ 'dark text-bodydark bg-boxdark-2': darkMode === true }">
     <div class=" bg-gray-100 dark:bg-[#1E293B] ">
         @include('client.layouts.navigation')
         <div class="duration-500 ease-in-out transform content ">

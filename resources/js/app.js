1912742d557d84ff55b88af85e87d424f09cd083
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2025-01-04 18:34:47 +0100

    Reinitiaisation de l'importation des scripts

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,26 +5,10 @@ import 'animate.css';
 import Swal from 'sweetalert2'
 import "./textarea"
 import 'jquery-validation';
-// import './home/sendMessage';
-// import './home/jquery';
-import './home/waypoints';
-import './home/bootstrap.bundle.min.js'
-import './home/swiper-bundle';
-import './home/slick';
-import './home/magnific-popup';
-import './home/nice-select';
-import './home/counterup';
-import './home/isotope-pkgd';
-import './home/imagesloaded-pkgd'
-import './home/countdown';
-import './home/ajax-form';
-import './home/meanmenu';
-import './home/jquery.knob';
-import './home/main';
-import './home/jquery-ui';
 
 window.$ = window.jQuery = $;
 window.Swal = Swal;
 window.Alpine = Alpine;
 
 Alpine.start();
+

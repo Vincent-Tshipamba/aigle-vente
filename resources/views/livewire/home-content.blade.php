bd1694ccba0e9e95054a902a2c545acb531562ab
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2025-01-13 17:45:38 +0100

    Ajout des prix des produits et des icones

--- resources/views/livewire/home-content.blade.php
+++ resources/views/livewire/home-content.blade.php
@@ -14,8 +14,7 @@
             <div class="swiper-wrapper grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 gap-4">
                 @foreach ($categories as $item)
                     <div class="swiper-slide">
-                        <a href="#"
-                            class="flex flex-col items-center text-center space-y-2 hover:scale-105">
+                        <a href="#" class="flex flex-col items-center text-center space-y-2 hover:scale-105">
                             <div class="p-2  rounded-full ">
                                 <img src="{{ $item->image }}" alt="{{ $item->name }}"
                                     class="w-8 h-8 max-w-full h-auto">
@@ -311,12 +310,13 @@
         @if ($products && $products->count() == 0)
             <div class="p-4 text-center justify-center w-[100%] mx-auto text-sm text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300"
                 role="alert">
-                <span class="font-medium">Oups désolé !</span> Aucun produit disponible pour le moment. Essayez de rafraichir la page s'il vous plait.
+                <span class="font-medium">Oups désolé !</span> Aucun produit disponible pour le moment. Essayez de
+                rafraichir la page s'il vous plait.
             </div>
         @else
             @foreach ($products as $index => $product)
                 <div class="w-72 rounded-xl duration-500">
-                    <a href="#">
+                    <a href="{{ route('products.show', $product->_id) }}">
                         <div class="image swiper-container product-swiper-{{ $index + 1 }}" loading="lazy">
                             <div class="swiper-wrapper">
                                 @foreach ($product->photos as $item)
@@ -337,21 +337,80 @@
                     </a>
                     <div class="px-4 py-3 w-72">
                         <span
-                            class="text-gray-400 mr-3 uppercase text-xs">{{ $product->category_product->name }}</span>
+                            class="text-gray-400 mr-3 uppercase text-xs">{{ $product->category_product->name }}</span><br>
+                        <span class="text-gray-400 mr-3 text-xs">Boutique {{ $product->shop->name }}</span>
+
                         <p class="text-lg font-bold text-black truncate block capitalize">{{ $product->name }}</p>
                         <div class="flex items-center">
-                            <p class="text-lg font-semibold text-black cursor-auto my-3">$149</p>
+                            <p class="text-lg font-semibold text-black cursor-auto my-3">${{ $product->unit_price }}
+                            </p>
                             <del>
-                                <p class="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+                                <p class="text-sm text-gray-600 cursor-auto ml-2">${{ $product->unit_price + 50 }}</p>
                             </del>
-                            <div class="ml-auto">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                    fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd"
-                                        d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
-                                    <path
-                                        d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                            <div class="ml-auto flex space-x-2">
+                                <!-- Contacter un vendeur -->
+                                <svg data-modal-target="contact-seller-modal" data-modal-toggle="contact-seller-modal"
+                                    data-tooltip-target="tooltip-contact-seller"
+                                    class="w-8 h-8 text-gray-800 dark:text-white hover:fill-[#e38407] hover:text-[#e38407] hover:cursor-pointer"
+                                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                                    height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M16 10.5h.01m-4.01 0h.01M8 10.5h.01M5 5h14a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1h-6.6a1 1 0 0 0-.69.275l-2.866 2.723A.5.5 0 0 1 8 18.635V17a1 1 0 0 0-1-1H5a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z" />
                                 </svg>
+                                <!-- Contact seller modal -->
+                                <div id="contact-seller-modal" data-modal-backdrop="static" tabindex="-1"
+                                    aria-hidden="true"
+                                    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                    <div class="relative p-4 w-full max-h-full">
+                                        <!-- Modal content -->
+                                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                            <!-- Modal header -->
+                                            <div
+                                                class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                                    Static modal
+                                                </h3>
+                                                <button type="button"
+                                                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                                    data-modal-hide="contact-seller-modal">
+                                                    <svg class="w-3 h-3" aria-hidden="true"
+                                                        xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 14 14">
+                                                        <path stroke="currentColor" stroke-linecap="round"
+                                                            stroke-linejoin="round" stroke-width="2"
+                                                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                                    </svg>
+                                                    <span class="sr-only">Close modal</span>
+                                                </button>
+                                            </div>
+                                            <!-- Modal body -->
+                                            <div class="p-4 md:p-5 space-y-4">
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="tooltip-contact-seller" role="tooltip"
+                                    class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                                    Envoyer un message au vendeur
+                                    <div class="tooltip-arrow" data-popper-arrow></div>
+                                </div>
+
+                                <!-- Ajouter a la wishlist -->
+                                <svg data-tooltip-target="tooltip-wishlist"
+                                    class="w-8 h-8 text-gray-800 dark:text-white hover:fill-[#e38407] hover:text-[#e38407] hover:cursor-pointer"
+                                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                                    height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z" />
+                                </svg>
+                                <div id="tooltip-wishlist" role="tooltip"
+                                    class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                                    Ajouter à ma wishlist
+                                    <div class="tooltip-arrow" data-popper-arrow></div>
+                                </div>
                             </div>
                         </div>
                     </div>

afc39262eb45465c5ae1c9fd9e2821b9d7bbfc61
mcleroi01 <musongelacarlo54@gmail.com>
2025-01-30 20:33:23 +0000

    mise a jour du card et le filter

--- resources/views/partials/home-partials/shop.blade.php
+++ resources/views/partials/home-partials/shop.blade.php
@@ -13,41 +13,25 @@
         <div class="shopareaitem">
             <div class="shopslider-active swiper-container">
                 <div class="swiper-wrapper">
-                    <div class="tpshopitem swiper-slide">
-                        <a class="popup-image" href="{{ asset('img/instagram/coming-soon.png') }}">
-                            <img loading="lazy" src="{{ asset('img/instagram/coming-soon.png') }}"
-                                alt="vignette-boutique">
-                        </a>
-                    </div>
-                    <div class="tpshopitem swiper-slide">
-                        <a class="popup-image" href="{{ asset('img/instagram/instagram-01.png') }}">
-                            <img loading="lazy" src="{{ asset('img/instagram/instagram-01.png') }}"
-                                alt="vignette-boutique">
-                        </a>
-                    </div>
-                    <div class="tpshopitem swiper-slide">
-                        <a class="popup-image" href="{{ asset('img/instagram/coming-soon.png') }}">
-                            <img loading="lazy" src="{{ asset('img/instagram/coming-soon.png') }}"
-                                alt="vignette-boutique">
-                        </a>
-                    </div>
-                    <div class="tpshopitem swiper-slide">
-                        <a class="popup-image" href="{{ asset('img/instagram/instagram-02.jpg') }}">
-                            <img loading="lazy" src="{{ asset('img/instagram/instagram-02.jpg') }}"
-                                alt="vignette-boutique">
-                        </a>
-                    </div>
-                    <div class="tpshopitem swiper-slide">
-                        <a class="popup-image" href="{{ asset('img/instagram/coming-soon.png') }}">
-                            <img loading="lazy" src="{{ asset('img/instagram/coming-soon.png') }}"
-                                alt="vignette-boutique">
-                        </a>
-                    </div>
-                    <div class="tpshopitem swiper-slide">
-                        <a class="popup-image" href="{{ asset('img/instagram/coming-soon.png') }}">
-                            <img loading="lazy" src="{{ asset('img/instagram/coming-soon.png') }}"
-                                alt="vignette-boutique">
-                        </a>
+                    <div
+                        class="bg-gradient-to-br swiper-slide from-purple-50 to-purple-100 rounded-2xl overflow-hidden shadow-lg transform transition-all duration-300 hover:scale-105">
+                        <div class="relative">
+                            <img src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
+                                alt="Wireless Headphones" class="w-full h-64 object-cover">
+                            <div
+                                class="absolute top-4 right-4 bg-[#e38407] text-white text-xl font-bold rounded-lg px-4 py-2">
+                                SAVE 30%</div>
+                        </div>
+                        <div class="p-6">
+                            <h3 class="text-xl font-semibold mb-2">Wireless Headphones</h3>
+                            <div class="flex items-center gap-3 mb-4">
+                                <span class="text-gray-500 line-through relative">$159.99</span>
+                                <span class="text-2xl font-bold text-[#e38407]">$111.99</span>
+                            </div>
+                            <button
+                                class="w-full bg-[#e38407] text-white py-3 rounded-lg font-medium hover:bg-purple-700 transition-colors">Buy
+                                Now</button>
+                        </div>
                     </div>
                 </div>
             </div>
